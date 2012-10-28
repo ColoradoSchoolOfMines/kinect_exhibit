3b3034cfe2d3332456f1f9836be49259896bf1a9
msweatt <sweatt.marshall@gmail.com>
2012-10-28 15:16:10 -0600

    Generates items. Throws java.util.concurrentModificationException on linked list. Not sure how to fix. -Marshall

---
+++ src/main/java/edu/mines/csci598/recycler/frontend/GameConstants.java
@@ -0,0 +1,11 @@
+package edu.mines.csci598.recycler.frontend;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Marshall
+ * Date: 10/28/12
+ * Time: 2:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameConstants {
+}
