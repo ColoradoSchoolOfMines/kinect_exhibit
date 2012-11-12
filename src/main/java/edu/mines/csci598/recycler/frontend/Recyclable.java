a4c6926f677aef53eb811e4cadb8042179342f02
Amanreet Bajwa <abajwa@mines.edu>
2012-11-12 09:40:35 -0700

    moved the collision logic into the recyclable class

--- src/main/java/edu/mines/csci598/recycler/frontend/Recyclable.java
+++ src/main/java/edu/mines/csci598/recycler/frontend/Recyclable.java
@@ -1,8 +1,11 @@
 package edu.mines.csci598.recycler.frontend;
 
 import edu.mines.csci598.recycler.frontend.graphics.Displayable;
+import edu.mines.csci598.recycler.frontend.graphics.Line;
+import edu.mines.csci598.recycler.frontend.graphics.Path;
 import edu.mines.csci598.recycler.frontend.graphics.Sprite;
 import edu.mines.csci598.recycler.frontend.utils.GameConstants;
+import edu.mines.csci598.recycler.frontend.utils.Log;
 
 /**
  * Recyclables are things like bottles, plastic etc. that you would be swiping at.
@@ -45,5 +48,47 @@ public class Recyclable implements Displayable {
 
     public MotionState getCurrentMotion() {
         return currentMotion;
+    }
+
+    /**
+     * checks for a collision with the given hand
+     *
+     * @param hand
+     * @param currentTimeSec
+     * @return
+     */
+    public boolean hasCollisionWithHand(Hand hand, double currentTimeSec) {
+        if (sprite.getState() == Sprite.TouchState.TOUCHABLE) {
+            if(sprite.isPointInside(hand.getX(), hand.getY())) {
+
+                if (hand.getVelocityX() > GameConstants.MIN_HAND_VELOCITY) {
+                    Path path = new Path();
+                    Log.logInfo("INFO: Pushed Right");
+                    Line collideLine = new Line(sprite.getX(), sprite.getY(),
+                            sprite.getX() + GameConstants.ITEM_PATH_END, sprite.getY(),
+                            GameConstants.ITEM_PATH_TIME);
+                    path.addLine(collideLine);
+                    sprite.setPath(path);
+                    sprite.setStartTime(currentTimeSec);
+                    setMotionState(Recyclable.MotionState.FALL_RIGHT);
+                    return true;
+
+                    //r.getSprite().setHorizontalVelocity(GameConstants.HORIZONTAL_VELOCITY);
+                } else if (hand.getVelocityX() < -1 * GameConstants.MIN_HAND_VELOCITY) {
+                    Path path = new Path();
+                    Log.logInfo("INFO: Pushed Left");
+                    Line collideLine = new Line(sprite.getX(), sprite.getY(),
+                            sprite.getX() - GameConstants.ITEM_PATH_END, sprite.getY(),
+                            GameConstants.ITEM_PATH_TIME);
+                    path.addLine(collideLine);
+                    sprite.setPath(path);
+                    sprite.setStartTime(currentTimeSec);
+                    setMotionState(Recyclable.MotionState.FALL_LEFT);
+                    //r.getSprite().setHorizontalVelocity(-1 * GameConstants.HORIZONTAL_VELOCITY);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
