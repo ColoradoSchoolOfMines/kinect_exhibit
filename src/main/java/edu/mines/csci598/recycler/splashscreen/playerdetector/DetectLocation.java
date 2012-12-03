f17ec22d10746e8067b33e815e0d2d36f3d30fc5
Jimmie Aaron Butler <jimmiebtlr@gmail.com>
2012-12-02 23:37:42 -0700

    Add javacv files.

--- src/main/java/edu/mines/csci598/recycler/splashscreen/playerdetector/DetectLocation.java
+++ src/main/java/edu/mines/csci598/recycler/splashscreen/playerdetector/DetectLocation.java
@@ -1,15 +1,15 @@
 package edu.mines.csci598.recycler.splashscreen.playerdetector;
 
+import com.googlecode.javacv.ObjectFinder;
 import edu.mines.csci598.recycler.backend.GameManager;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jimmie
- * Date: 12/2/12
- * Time: 6:02 PM
- * To change this template use File | Settings | File Templates.
- */
+import java.awt.*;
+
 public class DetectLocation extends PlayerDetector{
+    Polygon _area;
+    boolean _inWatchedArea;
+    Thread _areaWatcher;
+    ObjectFinder _finder;
 
     public DetectLocation( long waitTime, GameManager manager){
         super( waitTime, manager );
