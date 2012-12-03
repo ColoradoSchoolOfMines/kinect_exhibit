236d9152de8f659d687919bc4e158583cfa44fe0
asutermo <asutermo@mymail.mines.edu>
2012-12-03 11:46:14 -0700

    instruction header is now a "splash screen section"

--- src/main/java/edu/mines/csci598/recycler/splashscreen/headers/InstructionHeader.java
+++ src/main/java/edu/mines/csci598/recycler/splashscreen/headers/InstructionHeader.java
@@ -12,26 +12,20 @@ public class InstructionHeader implements SplashScreenSection {
     private Image image;
 
     public InstructionHeader() {
-        image = new ImageIcon("src/main/resources/SpriteImages/FinalSpriteImages/instructions_half.png").getImage();
-        JLabel imageLabel = new JLabel();
-        imageLabel.setIcon(new ImageIcon(image));
-    }
+        image =  new ImageIcon("src/main/resources/SpriteImages/FinalSpriteImages/instructions_half.png").getImage();
 
-    public static void main(String[] arg) {
     }
 
     @Override
     public void initialize(Point topLeft, Point bottomRight, UpdateScreenCallback callback) {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public void draw(Graphics2D g) {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public void stop() {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
+
