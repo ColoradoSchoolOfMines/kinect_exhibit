0b3d281b73b056c4baa8044205969167358d7e4e
asutermo <asutermo@mymail.mines.edu>
2012-12-03 10:01:37 -0700

    added some documentation.

--- src/main/java/edu/mines/csci598/recycler/splashscreen/weather/LocalAreaWeather.java
+++ src/main/java/edu/mines/csci598/recycler/splashscreen/weather/LocalAreaWeather.java
@@ -20,10 +20,11 @@ public class LocalAreaWeather {
     private static final Logger log = Logger.getLogger(LocalAreaWeather.class.getName());
     private static final String API_KEY = "b665b08214224103123010";
     private static final String LOCAL_ZIP = "80401";
+    private static final String BASE_URL = "http://free.worldweatheronline.com/feed/weather.ashx";
 
     public static WeatherInformation retrieveWeatherInformation() {
         try {
-            URL weatherUrl = new URL("http://free.worldweatheronline.com/feed/weather.ashx?key="+API_KEY+"&q="+LOCAL_ZIP+"&num_of_days=1&format=xml");
+            URL weatherUrl = new URL(BASE_URL+"?key="+API_KEY+"&q="+LOCAL_ZIP+"&num_of_days=1&format=xml");
             URLConnection weatherConnection = weatherUrl.openConnection();
             InputStream weatherStream = weatherConnection.getInputStream();
             return parseWeatherXML(weatherStream);
@@ -46,43 +47,45 @@ public class LocalAreaWeather {
         doc.getDocumentElement().normalize();
         NodeList conditions = doc.getElementsByTagName("current_condition");
 
-        String windDegree;
-        String windSpeed;
-        String cloudCover;
-        String precipitation;
-        String temperature;
-        String pressure;
-        String humidity;
-        String visibility;
-
         for (int node = 0; node < conditions.getLength(); node++) {
             Node condition = conditions.item(node);
 
             if (condition.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element)condition;
-
-                windDegree = getTagValue("winddirDegree", element);
-                windSpeed = getTagValue("windspeedMiles", element);
-                pressure = getTagValue("pressure", element);
-                humidity = getTagValue("humidity", element);
-                visibility = getTagValue("visibility", element);
-                cloudCover = getTagValue("cloudcover", element);
-                precipitation = getTagValue("precipMM", element);
-                temperature = getTagValue("temp_C", element);
-
-                return new WeatherInformation(
-                        Integer.valueOf(windSpeed),
-                        Integer.valueOf(visibility),
-                        Integer.valueOf(windDegree),
-                        Integer.valueOf(cloudCover),
-                        Double.valueOf(precipitation),
-                        Integer.valueOf(temperature),
-                        Integer.valueOf(pressure),
-                        Integer.valueOf(humidity)
-                );
+                return buildWeatherObject((Element) condition);
             }
         }
         return null;
+    }
+
+    private static WeatherInformation buildWeatherObject(Element condition) {
+        String windDegree;
+        String windSpeed;
+        String pressure;
+        String humidity;
+        String visibility;
+        String cloudCover;
+        String precipitation;
+        String temperature;
+
+        windDegree = getTagValue("winddirDegree", condition);
+        windSpeed = getTagValue("windspeedMiles", condition);
+        pressure = getTagValue("pressure", condition);
+        humidity = getTagValue("humidity", condition);
+        visibility = getTagValue("visibility", condition);
+        cloudCover = getTagValue("cloudcover", condition);
+        precipitation = getTagValue("precipMM", condition);
+        temperature = getTagValue("temp_C", condition);
+
+        return new WeatherInformation(
+                Integer.valueOf(windSpeed),
+                Integer.valueOf(visibility),
+                Integer.valueOf(windDegree),
+                Integer.valueOf(cloudCover),
+                Double.valueOf(precipitation),
+                Integer.valueOf(temperature),
+                Integer.valueOf(pressure),
+                Integer.valueOf(humidity)
+        );
     }
 
     private static String getTagValue(String sTag, Element eElement) {
