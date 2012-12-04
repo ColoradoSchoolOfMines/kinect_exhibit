0b26792df24116097f97f1d5721f065f0ccf2d13
Alec G <throwpi@me.com>
2012-12-03 23:07:35 -0700

    Merging InstructionHeader

--- src/main/java/edu/mines/csci598/recycler/splashscreen/headers/InstructionHeader.java
+++ src/main/java/edu/mines/csci598/recycler/splashscreen/headers/InstructionHeader.java
@@ -7,26 +7,35 @@ import edu.mines.csci598.recycler.splashscreen.graphics.UpdateScreenCallback;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.ImageObserver;
 
 public class InstructionHeader implements SplashScreenSection {
 
     private Image image;
+    private ImageObserver imageObserver;
+    private UpdateScreenCallback callback;
+    private Point topLeft;
+    private Point bottomRight;
 
-    public InstructionHeader() {
+    public InstructionHeader(Component imageObserver) {
         image =  new ImageIcon("src/main/resources/SpriteImages/FinalSpriteImages/instructions_half.png").getImage();
-
+        this.imageObserver = imageObserver;
     }
 
     @Override
     public void initialize(Point topLeft, Point bottomRight, UpdateScreenCallback updateScreenCallback, CycleScreenCallback cycleScreenCallback) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
     }
 
     @Override
     public void draw(Graphics2D g) {
+        g.drawImage(image, topLeft.x, topLeft.y, imageObserver);
     }
 
     @Override
     public void stop() {
+
     }
 }
 
