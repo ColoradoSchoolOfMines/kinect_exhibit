15748c586952b6286f8abc474345313d9eaa3147
Jerry Ramey <jerry.ramey@codefutures.com>
2012-12-05 16:42:17 -0700

    COmmits

--- src/main/java/edu/mines/csci598/recycler/frontend/GameOver.java
+++ src/main/java/edu/mines/csci598/recycler/frontend/GameOver.java
@@ -34,6 +34,9 @@ public class GameOver {
             gameStatusDisplay.setGameOverState(true);
             gameScreen.addSprite(s);
             displayed = true;
+        }else{
+            gameScreen.removeSprite(s);
+            gameScreen.addSprite(s);
         }
     }
 }
